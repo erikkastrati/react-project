08f03ef4db46e6d9bf0660e56b7f0b2b6a74d16d
Erik Kastrati <erikkastrati1999@gmail.com>
2023-12-12 22:36:49 +0100

    Komiti pare

---
+++ web-react/src/App.js
@@ -0,0 +1,24 @@
+// App.js
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import About from "./pages/About";
+import Login from "./pages/Signup";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
